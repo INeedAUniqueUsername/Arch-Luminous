acfbf79424f3d363ff8b48bbefb6e918060a119f
INeedAUniqueUsername <achen115@ucr.edu>
2018-02-28 15:09:25 -0800

    Reorder commands and add help

--- cerebrum.js
+++ cerebrum.js
@@ -3,16 +3,99 @@ const lisp = CONFIG.lisp ? require(CONFIG.lisp) : {};
 const core = require('./core.js');
 const rpg = require('./rpg.js');
 let sponges = {}, userVars = {}, macros = {};
+let users = [];
+const prefix = 'A!';
 module.exports = {
-    prefix: 'A!',
+    commands: {
+        bind: function(message, args) {
+            let author = message.author.id;
+            if(author !== CONFIG.archId) {
+                return;
+            }
+            let alias = args.shift();
+            let macro = args.join(' ');
+            if(!alias) {
+                message.channel.send(core.tag(message.author.id) + ', missing alias parameter');
+            } else if(!macro) {
+                message.channel.send(core.tag(message.author.id) + ', missing macro parameter');
+            } else if(module.exports.commands[alias]) {
+                message.channel.send(core.tag(message.author.id) + ', cannot override core functionality');
+            } else {
+                macros[alias] = macro;
+                message.channel.send(core.tag(message.author.id) + ', bound `' + alias + ' -> ' + macro + '`');
+            }
+        },
+        bindings: function(message, args) {
+            let reply = '';
+            let criterion = args[0] || '';
+            for(let key in macros) {
+                if(key.includes(criterion) || macros[key].includes(criterion)) {
+                    reply += '\n' + '`' + key + ' -> ' + macros[key] + '`';
+                }
+            }
+            message.channel.send(core.tag(message.author.id) + ', ' + reply);
+        },
+        lisp: function(message, args) {
+            args = args.join(' ');
+            try {
+                let result = lisp.run(args.trim());
+                if(Array.isArray(result)) {
+                    result = lisp.decode(result);
+                }
+                message.channel.send(core.tag(message.author.id) + ', ' + result);
+            } catch(error) {
+                message.channel.send(core.tag(message.author.id) + ', ' + error);
+            }
+        },
+        sponge: function(message, args) {
+            message.channel.send(('' + core.tag(message.author.id) + ', you have been ')
+                + ((sponges[message.author.id] = !sponges[message.author.id]) ? 'polymorphed into a sponge!' : 'polymorphed back to normal!'));
+        },
+        unbind: function(message, args) {
+            let author = message.author.id;
+            if(author !== CONFIG.archId) {
+                return;
+            }
+            //The alias already got replaced with a macro
+            let alias = args.join(' ');
+            if(!alias) {
+                message.channel.send(core.tag(message.author.id) + ', missing alias parameter');
+            } else {
+                for(let alias_macro in macros) {
+                    if(alias_macro === alias) {
+                        message.channel.send(core.tag(message.author.id) + ', unbound `' + alias + ' -> ' + macros[alias] + '`');
+                        delete macros[alias];
+                        return;
+                    }
+                }
+                message.channel.send(core.tag(message.author.id) + ', unknown alias `' + alias + '`');
+            }
+        },
+    },
+    help: {
+        bind: ('`' + prefix + 'bind <alias> <macro>`: Binds a custom command macro for your convenience'),
+        bindings: ('`' + prefix + 'bindings`: Lists all active macros.'),
+        lisp: ('`' + prefix + 'lisp`: Runs lisp code'),
+        sponge: ('`' + prefix + 'sponge`: Transforms you into a sponge'),
+        unbind: ('`' + prefix + 'unbind <alias>`: Unbinds a custom command macro for your convenience')
+    },
+    macros: macros,
+    prefix: prefix,
     ready: function() {
         console.log('Cerebrum online');
     },
     respond: function(message) {
+        if(!users.includes(message.author.id)) {
+            users.push(message.author.id);
+        }
+        
         let input = message.content;
         let author = message.author.id;
         let input2;
-        if(input.toLowerCase() === 'hello') {
+        //If the user is pinging someone
+        if(users.includes(core.id(input))) {
+            message.channel.send('***HEY ' + input + ', SOMEONE IS CALLING FOR YOU!***');
+        } else if(input.toLowerCase() === 'hello') {
             message.channel.send("world!");
         } else if(!input.startsWith(module.exports.prefix + 'sponge') && sponges[message.author.id]) {
             message.channel.send(core.tag(message.author.id) + ', *whoa, a talking sponge?!*');
@@ -44,71 +127,4 @@ module.exports = {
         }
         return true;
     },
-    commands: {
-        sponge: function(message, args) {
-            message.channel.send(('' + core.tag(message.author.id) + ', you have been ')
-                + ((sponges[message.author.id] = !sponges[message.author.id]) ? 'polymorphed into a sponge!' : 'polymorphed back to normal!'));
-        },
-        lisp: function(message, args) {
-            args = args.join(' ');
-            try {
-                let result = lisp.run(args.trim());
-                if(Array.isArray(result)) {
-                    result = lisp.decode(result);
-                }
-                message.channel.send(core.tag(message.author.id) + ', ' + result);
-            } catch(error) {
-                message.channel.send(core.tag(message.author.id) + ', ' + error);
-            }
-        },
-        bind: function(message, args) {
-            let author = message.author.id;
-            if(author !== CONFIG.archId) {
-                return;
-            }
-            let alias = args.shift();
-            let macro = args.join(' ');
-            if(!alias) {
-                message.channel.send(core.tag(message.author.id) + ', missing alias parameter');
-            } else if(!macro) {
-                message.channel.send(core.tag(message.author.id) + ', missing macro parameter');
-            } else if(module.exports.commands[alias]) {
-                message.channel.send(core.tag(message.author.id) + ', cannot override core functionality');
-            } else {
-                macros[alias] = macro;
-                message.channel.send(core.tag(message.author.id) + ', bound `' + alias + ' -> ' + macro + '`');
-            }
-        },
-        unbind: function(message, args) {
-            let author = message.author.id;
-            if(author !== CONFIG.archId) {
-                return;
-            }
-            //The alias already got replaced with a macro
-            let alias = args.join(' ');
-            if(!alias) {
-                message.channel.send(core.tag(message.author.id) + ', missing alias parameter');
-            } else {
-                for(let alias_macro in macros) {
-                    if(alias_macro === alias) {
-                        message.channel.send(core.tag(message.author.id) + ', unbound `' + alias + ' -> ' + macros[alias] + '`');
-                        delete macros[alias];
-                        return;
-                    }
-                }
-                message.channel.send(core.tag(message.author.id) + ', unknown alias `' + alias + '`');
-            }
-        },
-        bindings: function(message, args) {
-            let reply = '';
-            let criterion = args[0] || '';
-            for(let key in macros) {
-                if(key.includes(criterion) || macros[key].includes(criterion)) {
-                    reply += '\n' + '`' + key + ' -> ' + macros[key] + '`';
-                }
-            }
-            message.channel.send(core.tag(message.author.id) + ', ' + reply);
-        }
-    },
-    macros: macros,
 };
